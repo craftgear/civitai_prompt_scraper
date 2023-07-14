12f9e797e891265225fd18fc63643375fe3ae46c
Shunsuke Watanabe <craftgear@gmail.com>
2023-07-14 14:18:19 +0900

    add specs

---
+++ src/utils/dom.test.ts
@@ -0,0 +1,35 @@
+import { afterAll, beforeAll, describe, expect, it, Mock, vi } from 'vitest';
+import { parseNextData, waitForElement } from './dom';
+
+import { selector } from '../infra/dom';
+vi.mock('../infra/dom', () => ({
+  selector: vi.fn(),
+}));
+
+describe('waitForElement', () => {
+  beforeAll(() => {
+    vi.useFakeTimers();
+  });
+
+  afterAll(() => {
+    vi.useRealTimers();
+  });
+
+  it('waits for an element rendered', async () => {
+    (selector as Mock).mockReturnValueOnce(undefined).mockReturnValueOnce(true);
+    expect(waitForElement('hoge')).resolves.toEqual(true);
+    await vi.advanceTimersByTimeAsync(2001);
+  });
+
+  it('returned null when retry count is 0', async () => {
+    (selector as Mock)
+      .mockReturnValueOnce(undefined)
+      .mockReturnValueOnce(undefined);
+    expect(waitForElement('hoge', 2)).resolves.toEqual(null);
+    await vi.advanceTimersByTimeAsync(2001);
+  });
+});
+
+describe('parseNextData', () => {
+  it('');
+});
